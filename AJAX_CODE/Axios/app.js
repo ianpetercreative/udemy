4a4bfcba76d892d086ba785f8c35efdf7606fc44
ianmacdonald <ianpetercreative@gmail.com>
2024-02-12 18:15:30 -0700

    axios practice

--- AJAX_CODE/Axios/app.js
+++ AJAX_CODE/Axios/app.js
@@ -21,4 +21,25 @@
 
 
 // CODEALONG
+// https://swapi.dev/api/people/1/
 
+
+// axios.get('https://swapi.dev/api/people/1/')
+//   .then(res => {
+//     console.log(res)
+//   })
+//   .catch(err => {
+//     console.log(err)
+//   })
+
+
+const getStarWarsPerson = async(id) => {
+  try{
+    const res = await axios.get(`https://swapi.dev/api/people/${id}/`)
+    console.log(res.data)
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+getStarWarsPerson(55)
