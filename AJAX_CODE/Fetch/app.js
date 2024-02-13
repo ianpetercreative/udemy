59af84174ad5fd90585effb20d5fa9a4ff838d8d
ianmacdonald <ianpetercreative@gmail.com>
2024-02-12 17:45:00 -0700

    http requests practice

--- AJAX_CODE/Fetch/app.js
+++ AJAX_CODE/Fetch/app.js
@@ -1,26 +1,6 @@
-fetch("https://swapi.dev/api/people/1/")
-  .then((res) => {
-    console.log("RESOLVED!", res);
-    return res.json();
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((e) => {
-    console.log("ERROR!", e);
-  });
-
 // fetch("https://swapi.dev/api/people/1/")
 //   .then((res) => {
 //     console.log("RESOLVED!", res);
-//     return res.json();
-//   })
-//   .then((data) => {
-//     console.log(data);
-//     return fetch("https://swapi.dev/api/people/2/");
-//   })
-//   .then((res) => {
-//     console.log("SECOND REQUEST RESOLVED!!!");
 //     return res.json();
 //   })
 //   .then((data) => {
@@ -30,17 +10,67 @@ fetch("https://swapi.dev/api/people/1/")
 //     console.log("ERROR!", e);
 //   });
 
-const loadStarWarsPeople = async () => {
-  try {
-    const res = await fetch("https://swapi.dev/api/people/1/");
-    const data = await res.json();
-    console.log(data);
-    const res2 = await fetch("https://swapi.dev/api/people/2/");
-    const data2 = await res2.json();
-    console.log(data2);
-  } catch (e) {
-    console.log("ERROR!!!", e);
-  }
-};
+// // fetch("https://swapi.dev/api/people/1/")
+// //   .then((res) => {
+// //     console.log("RESOLVED!", res);
+// //     return res.json();
+// //   })
+// //   .then((data) => {
+// //     console.log(data);
+// //     return fetch("https://swapi.dev/api/people/2/");
+// //   })
+// //   .then((res) => {
+// //     console.log("SECOND REQUEST RESOLVED!!!");
+// //     return res.json();
+// //   })
+// //   .then((data) => {
+// //     console.log(data);
+// //   })
+// //   .catch((e) => {
+// //     console.log("ERROR!", e);
+// //   });
 
-loadStarWarsPeople();
+// const loadStarWarsPeople = async () => {
+//   try {
+//     const res = await fetch("https://swapi.dev/api/people/1/");
+//     const data = await res.json();
+//     console.log(data);
+//     const res2 = await fetch("https://swapi.dev/api/people/2/");
+//     const data2 = await res2.json();
+//     console.log(data2);
+//   } catch (e) {
+//     console.log("ERROR!!!", e);
+//   }
+// };
+
+// loadStarWarsPeople();
+
+
+// CODEALONG
+
+// fetch("https://swapi.dev/api/people/1/")
+//   .then((res) => {
+//     console.log('Resolved', res)
+//     return res.json()
+//   })
+//   .then((data) => {
+//     console.log(data)
+//   })
+//   .catch((err) => {
+//     console.log("error", err)
+//   })
+
+const loadSWPeople = async () => {
+  try{
+    const res = await fetch ('https://swapi.dev/api/people/1/')
+    const data = await res.json();
+    console.log(data)
+    const res2 = await fetch ('https://swapi.dev/api/people/2/')
+    const data2 = await res2.json();
+    console.log(data2)
+  } catch (err) {
+    console.log("Error: ", err)
+  }
+}
+
+loadSWPeople(); 
--- AJAX_CODE/XMLHTTP/app.js
+++ AJAX_CODE/XMLHTTP/app.js
@@ -1,19 +1,21 @@
 // NOT AT ALL IMPORTANT TO REMEMBER ANY OF THIS CODE!
 
-// const req = new XMLHttpRequest();
+const req = new XMLHttpRequest();
 
-// req.onload = function () {
-//   console.log("IT LOADED!!");
-//   const data = JSON.parse(this.responseText);
-//   console.log(data.name, data.height);
-// };
+req.onload = function () {
+  console.log("IT LOADED!!");
+  console.log("This: ");
+  console.log(this);
+  const data = JSON.parse(this.responseText);
+  console.log(data.name, data.height);
+};
 
-// req.onerror = function () {
-//   console.log("ERROR!!!!");
-//   console.log(this);
-// };
+req.onerror = function () {
+  console.log("ERROR!!!!");
+  console.log(this);
+};
 
-// req.open("GET", "https://swapi.dev/api/people/1/");
-// req.send();
+req.open("GET", "https://swapi.dev/api/people/1/");
+req.send();
 
 
