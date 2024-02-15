7e9cc3dbab519a3d42920dd30717928450b8ec0a
ianmacdonald <ianpetercreative@gmail.com>
2024-02-14 20:20:24 -0700

    oop practice

--- OOP_Code/05_Classes/app.js
+++ OOP_Code/05_Classes/app.js
@@ -1,26 +1,60 @@
+// class Color {
+// 	constructor(r, g, b, name) {
+// 		this.r = r;
+// 		this.g = g;
+// 		this.b = b;
+// 		this.name = name;
+// 	}
+// 	innerRGB() {
+// 		const { r, g, b } = this;
+// 		return `${r}, ${g}, ${b}`;
+// 	}
+// 	rgb() {
+// 		return `rgb(${this.innerRGB()})`;
+// 	}
+// 	rgba(a = 1.0) {
+// 		return `rgba(${this.innerRGB()}, ${a})`;
+// 	}
+// 	hex() {
+// 		const { r, g, b } = this;
+// 		return (
+// 			'#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
+// 		);
+// 	}
+// }
+// const red = new Color(255, 67, 89, 'tomato');
+// const white = new Color(255, 255, 255, 'white');
+
+
+// CLASS PRACTICE 
+
 class Color {
 	constructor(r, g, b, name) {
 		this.r = r;
 		this.g = g;
 		this.b = b;
-		this.name = name;
+		this.name = name
 	}
+
 	innerRGB() {
-		const { r, g, b } = this;
-		return `${r}, ${g}, ${b}`;
+		const { r, g, b } = this
+		return `${r}, ${g}, ${b}`
 	}
+
 	rgb() {
 		return `rgb(${this.innerRGB()})`;
 	}
+
 	rgba(a = 1.0) {
-		return `rgba(${this.innerRGB()}, ${a})`;
+		return `rgba(${this.innerRGB()}, ${a})`
 	}
+
 	hex() {
 		const { r, g, b } = this;
-		return (
-			'#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
-		);
+		return ('#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1));
+
 	}
 }
-const red = new Color(255, 67, 89, 'tomato');
+
+const tomato = new Color(255, 67, 85, 'tomato');
 const white = new Color(255, 255, 255, 'white');
